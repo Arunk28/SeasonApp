374f2da256e61df1202ed5f5fe58d9fc7d7261a7
Arunk28 <arunkumar280491@gmail.com>
2020-11-17 16:35:15 +0530

    life cycle methods excersise

--- src/index.js
+++ src/index.js
@@ -1,35 +1,27 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
 
 //class component excersise
-class App extends React.Component{
-    constructor(props){
-        super(props)
+class App extends React.Component {
+  state = { lat: null, error: "" };
 
-        this.state ={lat:null,error:""}
-
-        window.navigator.geolocation.getCurrentPosition(
-            position=>{
-                this.setState({lat:position.coords.latitude})
-            },
-            err=>{
-                this.setState({error:err.message})
-            }
-        );
-
+  //lifecycle method component did mount
+  componentDidMount() {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => this.setState({ lat: position.coords.latitude }),
+      (err) => this.setState({ error: err.message }),
+    );
+  }
+  render() {
+    if (!this.state.lat && this.state.error) {
+      return <div>Error : {this.state.error}</div>;
     }
-    render(){
-        if(!this.state.lat && this.state.error)
-        {
-        return <div>Error : {this.state.error}</div>
-        }
-        if(this.state.lat && !this.state.error)
-        {
-        return <div>latitude : {this.state.lat}</div>
-        }
-
-        return <div>loading...</div>
+    if (this.state.lat && !this.state.error) {
+      return <div>latitude : {this.state.lat}</div>;
     }
+
+    return <div>loading...</div>;
+  }
 }
 //render the jsx
-ReactDOM.render(<App/>,document.querySelector('#root'))
+ReactDOM.render(<App />, document.querySelector("#root"));
